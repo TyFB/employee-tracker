1f76bd1d5cc87fa6d42ff77d01308f8358fc5a02
TyFB <tfb1223@gmail.com>
2022-10-31 11:18:49 -0500

    serverside setup

--- server.js
+++ server.js
@@ -1,10 +1,98 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'R00tP4ss69',
+        database: 'managment'
+    },
+    console.log('Connected to the database.')
+);
+
+app.get('/api/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+
+    });
+});
+
+app.get('/api/employee/:id', (req, res) => {
+    const sql = `SELECT * FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+
+// Create a employee
+app.post('/api/employee', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO employee (first_name, last_name)
+                    VALUES (?,?)`;
+    const params = [body.first_name, body.last_name];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
